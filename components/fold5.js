ba82ad62f5637a825bf516b5a61d8df1a304971e
AKalyanPavan <pavanaravelli690@gmail.com>
2025-06-18 19:41:33 -0400

    build errors

--- components/fold5.js
+++ components/fold5.js
@@ -177,7 +177,7 @@ export default function Fold5() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <h2 className="text-3xl md:text-4xl font-bold text-gray-900 text-center mb-18">
-          What's trending
+          What&apos;s trending
         </h2>
 
         {/* Products Grid */}
